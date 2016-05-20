77312ec6e52852930d2a7e32fa12d474b3b04f6a
Ricardo Reiter <ricardof.reiter@gmail.com>
2016-05-20 15:31:40 -0300

    Created an angular directive to handle keyevents in an easy way, just adding two attributes to your tag, "rr-key-press" and "rr-key-code".

---
+++ src/rrKeyEvent.js
@@ -0,0 +1,37 @@
+(function () { 
+"use strict";
+
+var module = angular.module('rrKeyEvent', []);
+var ENTER_KEY_CODE = 13;
+
+module.directive('rrEnterKeyPress', function() {
+    return {
+    		restrict: 'A',
+    		link: function($scope, $element, $attrs) {
+		        bindKeyPressEvent($scope, $element, $attrs.rrEnterKeyPress, ENTER_KEY_CODE);
+	    	}
+	};
+});
+
+module.directive('rrKeyPress', function() {
+    return {
+    		restrict: 'A',
+    		link: function($scope, $element, $attrs) {
+    			bindKeyPressEvent($scope, $element, $attrs.rrKeyPress, $attrs.rrKeyCode);
+    		}
+	};
+});
+
+function bindKeyPressEvent(scope, element, keyEvent, keyCode) {
+    element.bind("keypress keyup", function(event) {
+        var typedKeyCode = event.which || event.keyCode;
+
+        if (typedKeyCode == keyCode) {
+            scope.$apply(function() {
+                scope.$eval(keyEvent);
+            });
+        }
+    });
+}
+
+}());
